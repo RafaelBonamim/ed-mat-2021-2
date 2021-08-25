188236de684a3dc9633f66fda33b3bfcc2d49c07
3n1Gm4 <70954632+RafaelBonamim@users.noreply.github.com>
2021-08-24 22:03:59 -0300

    Add files via upload

---
+++ 03 Arrow Functions.js
@@ -0,0 +1,120 @@
+/*
+
+        _..._            ★ ° . *　　　°　.　°☆ 　. * ● ¸ .　 * 　.　 　★     ° :.☆
+      .'     '.      _   . 　　　★ 　° :. ★　 * • ○ ° ★　 . 　　　★ 　° :. 　 * • ○ ° ★　 
+     /    .-""-\   _/ \  .　 * 　.　 　　　　　. 　 ★ ° . *　　　°　.　°☆ 　. * ● ¸ 
+   .-|   /:.   |  |   |  ° 　. ● . ★ ° . *　　　°　.　°☆ ° . *　　　°　.　°☆ 　. * ● ¸ . 
+   |  \  |:.   /.-'-./   　. * ● ¸ . 　　　★ 　° :●. 　 * ° °☆ 　¸. ● . 　　★　★ 
+   | .-'-;:__.'    =/    • ○ ° ★　 .　 * 　.　 　　　　　.　★　　　　. 　 ° 　.  . 　    ★　 　　
+   .'=  *=|     _.='      　 ° 　. ● . ★ ° . *　　　°　.　° :. 　 * • ○ ° ★　 .　 * 　.　 
+  /   _.  |    ;       °☆ 　. * ● ¸ . 　　　★ 　°☆ 　. * ● ¸ . 　　　★ 　
+ ;-.-'|    \   |      ° :. 　 * • ○ ° ★　 .　 * 　.　  　 ° 　. ● . ★ ° . *　　　°　.　
+/   | \    _\  _\     　★　　　　. 　 ° 　.  . 　    ★　 　　• ○ ° ★　 .　 * 　.　 　　　　　.
+\__/'._;.  ==' ==\    ° °☆ 　¸. ● . 　　★　★ 　. * ● ¸ . 　　　★ 　° :●. 　 * 
+         \    \   |   ° . *　　　°　.　°☆ 　. * ● ¸ . ° 　. ● . ★ ° . *　　　°　.　°☆ 
+         /    /   /   ★ ° . *　　　°　.　°☆ 　. * ● ¸ .　 * 　.　 　　　　　. 　 
+         /-._/-._/   . 　　　★ 　° :. 　 * • ○ ° ★　 . 　　　★ 　° :. ★　 * • ○ ° ★　 
+         \   `\  \  .　 * 　.　 　★     ° :.☆★ ° . *　　　°　.　°☆ 　. * ● ¸ 
+          `-._/._/  
+
+              .........                     ╔═══╗╔═══╗╔════╗╔═══╗╔╗─╔╗╔════╗╔╗─╔╗╔═══╗╔═══╗
+            .'------.' |                    ║╔══╝║╔═╗║║╔╗╔╗║║╔═╗║║║─║║║╔╗╔╗║║║─║║║╔═╗║║╔═╗║
+           | .-----. | |                    ║╚══╗║╚══╗╚╝║║╚╝║╚═╝║║║─║║╚╝║║╚╝║║─║║║╚═╝║║║─║║
+           | |     | | |                    ║╔══╝╚══╗║──║║──║╔╗╔╝║║─║║──║║──║║─║║║╔╗╔╝║╚═╝║
+         __| |     | | |;. _______________  ║╚══╗║╚═╝║──║║──║║║╚╗║╚═╝║──║║──║╚═╝║║║║╚╗║╔═╗║
+        /  |*`-----'.|.' `;              // ╚═══╝╚═══╝──╚╝──╚╝╚═╝╚═══╝──╚╝──╚═══╝╚╝╚═╝╚╝─╚╝
+       /   `---------' .;'              //      ╔═══╗╔═══╗     ╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗
+ /|   /  .''''////////;'               //       ╚╗╔╗║║╔══╝     ╚╗╔╗║║╔═╗║╚╗╔╗║║╔═╗║║╔═╗║
+|=|  .../ ######### /;/               //|       ─║║║║║╚══╗     ─║║║║║║─║║─║║║║║║─║║║╚══╗
+|/  /  / ######### //                //||       ─║║║║║╔══╝     ─║║║║║╚═╝║─║║║║║║─║║╚══╗║
+   /   `-----------'                // ||       ╔╝╚╝║║╚══╗     ╔╝╚╝║║╔═╗║╔╝╚╝║║╚═╝║║╚═╝║
+  /________________________________//| ||       ╚═══╝╚═══╝     ╚═══╝╚╝─╚╝╚═══╝╚═══╝╚═══╝
+  `--------------------------------' | ||   
+   : | ||      | || |__LL__|| ||     | ||     █▀█ █▀█ █▀█ █▀▀ ░   █▀▀ ▄▀█ █░█ █▀ ▀█▀ █▀█
+   : | ||      | ||         | ||     `""'     █▀▀ █▀▄ █▄█ █▀░ ▄   █▀░ █▀█ █▄█ ▄█ ░█░ █▄█
+   n | ||      `""'         | ||              
+   M | ||                   | ||                       █▀▀ █ █▄░█ ▀█▀ █▀█ ▄▀█
+     | ||                   | ||                       █▄▄ █ █░▀█ ░█░ █▀▄ █▀█
+     `""'                   `""'
+*/
+
+
+// Função tradicional com 1 parâmetro e
+// 1 linha no corpo, com return
+
+function quadrado(x) {
+    return x * x
+}
+
+console.log(quadrado(7))
+
+// ARROW FUNCTION: forma "abreviada" de escrever funções em js
+
+// arrowfunction equivalente ao quadrado
+// 1) a função é atribuida a uma variável (ou ainda melhor, uma constante)
+// 2) desaparece a palavra-chave "function"
+// 3) desaparece os parênteses em torno do parâmetro
+// 4) entre o parâmetro e o corpo da função, temos o sinal "=>" (arrow, flecha)
+// 5) desaparecem as chaves que delimitam o corpo da função
+// 6) desaparece a palavra-chave "return"
+
+const quadrado2 = x => x * x
+
+console.log(quadrado2(7))
+
+console.log('=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= ')
+
+
+
+
+// Função tradicional com 2 parâmetros, 1 linha de corpo e return
+function potencia(base, expoente){
+    return base ** expoente
+}
+
+console.log(potencia(2,5))
+
+// Arrow function correspondente
+// retornam os parênteses em torno dos parâmetros
+
+const potencia2 = (base, expoente) => base **expoente
+
+console.log(potencia2(2,5))
+console.log('=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= ')
+
+
+
+
+//Função tradicional sem parâmetros, 1 linha de corpo e com return
+function dataHoraAtual(){
+    return new Date()
+}
+
+// Arrow function correspondente
+// Os parênteses vazios marcam a posição do parâmetro
+
+const dataHoraAtual2 = () => new Date ()
+
+console.log(dataHoraAtual())
+console.log(dataHoraAtual2())
+console.log('=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= ')
+
+// Função tradicional com 1 parâmetro e múltiplas linhas de corpo
+function fatorial(n){
+    let res = 1
+    for (let i = n; i > 1; i--) res *= i
+    return res
+}
+
+// Arrow function correspondente
+// Retornam as chaves delimitando o corpo da função
+
+const fatorial2 = n => {
+    let res = 1
+    for (let i = n; i > 1; i--) res *= i
+    return res
+}
+
+console.log(fatorial(5))
+console.log(fatorial2(5))
+console.log('=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= ')
